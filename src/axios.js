4900a66858c49e481401038f84121306962cd691
DungNg <87799163+nminhdung@users.noreply.github.com>
2023-11-09 14:56:50 +0700

    Update axios.js

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,8 @@ import axios from "axios";
 import { apiRefreshToken } from "./api";
 
 const instance = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+  // baseURL: process.env.REACT_APP_API_URL,
+  baseURL:"https://digital-mern-stacks-api.onrender.com/api"
 });
 //luu cookie ben server gui qua client
 instance.defaults.withCredentials = true;
