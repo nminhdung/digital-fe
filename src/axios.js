98fa4ea53694cf3db41a53ac791e4de614bb471c
DungNg <87799163+nminhdung@users.noreply.github.com>
2024-02-21 12:48:46 +0700

    fix url

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { apiRefreshToken } from "./api";
 
 const instance = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL:"https://digital-mern-stacks-api.onrender.com/api",
   // baseURL:'http://localhost:7000/api'
 });
 //luu cookie ben server gui qua client
